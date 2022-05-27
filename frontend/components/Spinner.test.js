aea4081719c440e6cfd595fe90dbf3105d0db293
unknown <macrowell@yahoo.com>
2022-05-27 07:34:25 -0400

    spinner renders successfully when on

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
+import React from 'react'
+import Spinner from './Spinner';
+import { render } from '@testing-library/react'
+
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
   expect(true).not.toBe(false)
+})
+
+test("spinner renders when on", () => {
+  render(<Spinner on={true}/>)
 })
